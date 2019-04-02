0a764da70af01936cf6c621607793f328cc47b1f
deahln <deahln@rpi.edu>
2019-04-02 12:29:11 -0400

    login server request

--- Webstack/Shout/src/app/login.service.ts
+++ Webstack/Shout/src/app/login.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private url = "http://localhost:3000"
+
+  logIn(username, password){
+    let auth = this.http.get(this.url + "/login?username="+username+"&password="+password);
+
+  }
 }
