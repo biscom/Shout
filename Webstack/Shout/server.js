8137aa4d6df2d1263b0f7f0fca7cb3c104745ae0
deahln <deahln@rpi.edu>
2019-04-08 22:57:04 -0400

    login requests

--- Webstack/Shout/server.js
+++ Webstack/Shout/server.js
@@ -3,7 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var session = require('client-sessions');
 const MongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 const uri = "mongodb+srv://testUser:ITWS4500@cluster0-tpsxu.mongodb.net/test?retryWrites=true";
 MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
@@ -16,8 +17,34 @@ MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
 	client.close();
  });
 
-app.post('/login', function(req, res){
-	
+app.post('/createAccount', function(req, res){
+	//check database for existing user info 
+	bcrypt.hash(res.password, null, function(err, hash) {
+		// Store account in database
+	});
+});
+
+app.post('/login', function(req,res){
+	username = res.username;
+	password = res.password;
+	MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
+		if(err) {
+			 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+		}
+		console.log('Connected...');
+		const collection = client.db("ITWS-4500").collection("accounts");
+		// get account info 
+		bcrypt.compare(password, db_hash,function(error, result){
+			if(res){
+
+			}else{
+
+			}
+		});
+
+		client.close();
+	 });
+
 });
 
 app.use(session({
