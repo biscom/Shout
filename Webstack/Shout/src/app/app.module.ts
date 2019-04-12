1ab4d4ee42714aecce7c4145b1e2beab0893cc70
hhy4285 <yi998hh@gmail.com>
2019-04-12 02:40:32 -0400

    use templateUrl

--- Webstack/Shout/src/app/app.module.ts
+++ Webstack/Shout/src/app/app.module.ts
@@ -2,25 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { ProfileComponent } from './profile/profile.component';
 import { TrendingComponent } from './trending/trending.component';
 import { UniversityComponent } from './university/university.component';
 
-// import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     ProfileComponent,
     TrendingComponent,
     UniversityComponent
-  ],
-  imports: [
-    // HttpClientModule,
-    BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
