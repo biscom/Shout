134f7ca8a0d86e7589608e32bee03958dce5170e
Michael Biscotti <biscom@rpi.edu>
2019-04-22 19:52:18 -0400

    updated post service

--- Webstack/Shout/src/app/posts.service.ts
+++ Webstack/Shout/src/app/posts.service.ts
@@ -12,6 +12,27 @@ export class PostsService {
   getTopPosts(tag, sortMethod){
     return this.http.get(this.url + "/top?tag="+tag+"&sortMethod="+sortMethod);
   }
+  
+  getTopUniversityPosts(tag, sortMethod){
+    return this.http.get(this.url + "/topUniversity?tag="+tag+"&sortMethod="+sortMethod);
+  }
+
+  addPost(user_id, msg_body, univid){
+    let postInfo={
+      user_id : user_id,
+      msg_body : msg_body,
+      univid : univid
+    };
+    return this.http.post(this.url+"/addPost", postInfo);
+  
+  addComment(user_id,msg_body, likes, dislikes){
+    let postInfo={
+      user_id : user_id,
+      msg_body : msg_body,
+      likes: likes,
+      dislikes: dislikes
+    };
+    return this.http.post(this.url+"/addPost", postInfo);
 
   getTaggedPosts(tag){
     return this.http.get(this.url+"/posts?tag="+tag);
