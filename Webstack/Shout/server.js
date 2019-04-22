5125a8fc9aba8ff75cd096776c2dce8cbdf30959
Michael Biscotti <biscom@rpi.edu>
2019-04-22 17:57:16 -0400

    fixed multiple connections to db

--- Webstack/Shout/server.js
+++ Webstack/Shout/server.js
@@ -25,72 +25,71 @@ app.use(cors({origin: [
 
 const uri = "mongodb+srv://testUser:ITWS4500@cluster0-tpsxu.mongodb.net/test?retryWrites=true";
 
-app.get('/currentUser', function(req,res){
-	var sessionInfo = {};
-	if(req.session.username){
-		sessionInfo.username = req.session.username;
-		MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
-			if(err) {
-				 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-			}
-			const collection = client.db("ITWS-4500").collection("University");
-			// perform actions on the collection object
-			collection.find({univid : req.session.univid}).toArray(function(err, result) {
-				sessionInfo.univid = result;
-			});
-		 });
-		 
-	}else{
-		sessionInfo.username = "Default";
-	}
-	res.json(sessionInfo);
-});
+MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
+    if(err) {
+         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+    }
+    
+    const db = client.db("ITWS-4500");
+    console.log("Connected ...")
+    
+    app.get('/currentUser', function(req,res){
+        var sessionInfo = {};
+        if(req.session.username){
+            sessionInfo.username = req.session.username;
 
-app.get('/nearby', function(req,res){
-	lat=req.query.lat;
-	lon=req.query.lon;
-	//searchdatabase for 3 closest unis and send back in json
-	MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
-		if(err) {
-			 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-		}
-		const collection = client.db("ITWS-4500").collection("University");
-		// perform actions on the collection object
-		collection.find({}).toArray(function(err, result) {
-			if (err) throw err;
-			result.sort(function(a,b){
-				alat = a.location.lat;
-				alon = a.location.lon;
-				blat = b.location.lat;
-				blon = b.location.lon;
-				abDist = function(c,d){
-					return Math.abs(c - d);
-				};
-				aDist= abDist(lat, alat) + abDist(lon, alon);
-				bDist = abDist(lat, blat)+ abDist(lon,blon);
-				return aDist - bDist
-			});
-			res.json(result.slice(0,5));
-		});
-	 });
+            const collection = db("ITWS-4500").collection("University");
 
-});
+            // perform actions on the collection object
+            collection.find({univid : req.session.univid}).toArray(function(err, result) {
+                sessionInfo.univid = result;
+            });
 
-app.post('/addpost', function(req, res){
-	//check database for existing user info 
-	user_id = req.body.user_id;
-	msg_body = req.body.msg_body;
-	univid = req.body.univid;
-        
-		// Store account in database
-        MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
-		if(err) {
-			 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-		}
-		console.log('Connected...');
-		const collection = client.db("ITWS-4500").collection("Post");
-            
-        
+        } else {
+            sessionInfo.username = "Default";
+        }
+        res.json(sessionInfo);
+    });
+
+    app.get('/nearby', function(req,res){
+        lat=req.query.lat;
+        lon=req.query.lon;
+
+        //searchdatabase for 3 closest unis and send back in json
+
+        const collection = db.collection("University");
+
+        // perform actions on the collection object
+        collection.find({}).toArray(function(err, result) {
+            if (err) throw err;
+            result.sort(function(a,b){
+                alat = a.location.lat;
+                alon = a.location.lon;
+                blat = b.location.lat;
+                blon = b.location.lon;
+                abDist = function(c,d){
+                    return Math.abs(c - d);
+                };
+                aDist= abDist(lat, alat) + abDist(lon, alon);
+                bDist = abDist(lat, blat)+ abDist(lon,blon);
+                return aDist - bDist
+            });
+            res.json(result.slice(0,5));
+        });
+
+
+    });
+
+    app.post('/addpost', function(req, res){
+        //check database for existing user info 
+        user_id = req.body.user_id;
+        msg_body = req.body.msg_body;
+        univid = req.body.univid;
+
+        // Store account in database
+
+        const collection = db.collection("Post");
+
         // a document instance
         var new_post = new Post({ user_id: user_id, msg_body: msg_body, univid: univid });
 
@@ -110,102 +109,88 @@ app.post('/addpost', function(req, res){
             reason: "Post has been created!"
         });
 
-            
-		  });
-        
-        });
+
+    });
+
+    app.post('/createAccount', function(req, res){
+        //check database for existing user info 
+        username = req.body.username;
+        nickname = req.body.nickname;
+        univ_email=req.body.univ_email;
+        univid = req.body.univid;
+        hash = bcrypt.hashSync(req.body.password, null);
 
 
-app.post('/createAccount', function(req, res){
-	//check database for existing user info 
-	username = req.body.username;
-	nickname = req.body.nickname;
-	univ_email=req.body.univ_email;
-	univid = req.body.univid;
-	hash = bcrypt.hashSync(req.body.password, null);
-        
-        
-		// Store account in database
-        MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
-		if(err) {
-			 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-		}
-		console.log('Connected...');
-		const collection = client.db("ITWS-4500").collection("User");
-            
+        // Store account in database
+
+        const collection = db.collection("User");
+
         collection.find(username).toArray(function(err, result) {
-			if (err){
+            if (err){
                 throw err;
             } 
-            
+
             if (result!= {} ){
-                
+
                 //Username already exists, return error to user
-				res.json({
-					success: false,
-					reason: "User already exists"
-				});
-                
+                res.json({
+                    success: false,
+                    reason: "User already exists"
+                });
+
             } else {
-                
+
                 // a document instance
                 var new_user = new User({ username: username, password: hash, nickname: nickname, email: univ_email, univid: univid });
 
-				// save model to database
-				try{
-					collection.insertOne(new_user);
-				}catch(e){
-					console.log(e);
+                // save model to database
+                try{
+                    collection.insertOne(new_user);
+                }catch(e){
+                    console.log(e);
                     res.json({
                         success: true,
                         reason: "Error: " + e
                     });
-				};
+                };
 
-				res.json({
-					success: true,
-					reason: "User "+ username + " has been created!"
-				});
-                
+                res.json({
+                    success: true,
+                    reason: "User "+ username + " has been created!"
+                });
             }
-		  });
-        
         });
-});
+    });
 
-//starts client session or returns false with info about incorrect info
-app.post('/login', function(req,res){
-	username = res.username;
-	password = res.password;
-	MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
-		if(err) {
-			 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-		}
-		console.log('Connected...');
-		const collection = client.db("ITWS-4500").collection("user");
-		// get account info 
-		//check if user exists
+    //starts client session or returns false with info about incorrect info
+    app.post('/login', function(req,res){
+        username = res.username;
+        password = res.password;
+
+        const collection = db.collection("User");
+        // get account info 
+        //check if user exists
         collection.find(username).toArray(function(err, result) {
-			if (err){
+            if (err){
                 throw err;
             } 
-            
+
             if (result != {} ){
-                
+
                 //Username or password are incorrect, return error to user
                 res.json({
-					success: false,
-					reason: "Username does not exist!"
-				});
-                
+                    success: false,
+                    reason: "Username does not exist!"
+                });
+
             } else {
-				user = result[0];
+                user = result[0];
                 //check if password matches
                 bcrypt.compare(password, user.password,function(error, result){
                     if(result){
                         //start client session 
-						req.session.username = username;
-						req.session.univid = user.univid;
+                        req.session.username = username;
+                        req.session.univid = user.univid;
                         res.json({
                             success: true,
                             reason: "User "+ username + " has logged in!"
@@ -219,93 +204,74 @@ app.post('/login', function(req,res){
                     }
                 });
             }
-	 });
+       });
+    });
+
+    //returns Top posts from everywhere
+    app.get('/top', function(req,res){
+        query = {};
+        if(req.query.tag != "all"){
+            query.tag = [req.query.tag];
+        }
+        sortMethod = req.query.sortMethod;
+
+        const collection = db.collection("Post");
+        // perform actions on the collection object
+
+        collection.find(query).toArray(function(err, result) {
+            if (err) throw err;
+            if(sortMethod == "recent"){
+                result.sort(function(a,b){
+                    return new Date(b.timestamp) - new Date(a.timestamp);
+                });
+            }else if(sortMethod == "popularity"){
+
+            }else if(sortMethod== "score"){
+                result.sort(function(a,b){
+                    aKarma = length(a.likes) - length(a.dislikes);
+                    bKarma = length(b.likes) - length(b.dislikes);
+                    return aKarma - bkarma;
+                });
+            }
+
+            res.json(result);
+        });
+    });
+
+    app.get('/users', function(req,res){
+        const collection = db.collection("User");
+        // perform actions on the collection object
+        collection.find({}).toArray(function(err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+    });
+
+    app.get('/profileInfo', function(req,res){
+        info={};
+        if(req.session.username){
+            info.username = req.session.username;
+            const collection = db.collection("User");
+            // perform actions on the collection object
+            collection.find({username: req.session.username}).toArray(function(err, result) {
+                if (err) throw err;
+                console.log(result[0]);
+                info.email = result[0].email;
+                res.json(info);			
+            });
+        } else {
+            info.username = "Not logged in";
+            info.email = "user@univ.edu";
+            res.json(info);
+        }
+    });
+
+    //returns posts with specific tag and univ id
+    app.get('/posts', function(req,res){
+        tag = req.query.tag;
+        univid=req.session.univid;
+
     });
 });
-
-//returns Top posts from everywhere
-app.get('/top', function(req,res){
-	query = {};
-	if(req.query.tag != "all"){
-		query.tag = [req.query.tag];
-	}
-	sortMethod = req.query.sortMethod;
-	console.log(query);
-
-	MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
-		if(err) {
-			 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-		}
-		console.log('Connected...');
-		const collection = client.db("ITWS-4500").collection("Post");
-		// perform actions on the collection object
-			
-		collection.find(query).toArray(function(err, result) {
-			if (err) throw err;
-			if(sortMethod == "recent"){
-				result.sort(function(a,b){
-					return new Date(b.timestamp) - new Date(a.timestamp);
-				});
-			}else if(sortMethod == "popularity"){
-				
-			}else if(sortMethod== "score"){
-				result.sort(function(a,b){
-					aKarma = length(a.likes) - length(a.dislikes);
-					bKarma = length(b.likes) - length(b.dislikes);
-					return aKarma - bkarma;
-				});
-			}
-			
-			res.json(result);
-		});
-	 });
-});
-
-app.get('/users', function(req,res){
-	MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
-		if(err) {
-			 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-		}
-		const collection = client.db("ITWS-4500").collection("User");
-		// perform actions on the collection object
-		collection.find({}).toArray(function(err, result) {
-			if (err) throw err;
-			res.json(result);
-		});
-	 });
-});
-
-app.get('/profileInfo', function(req,res){
-	info={};
-	if(req.session.username){
-		info.username = req.session.username;
-		MongoClient.connect(uri,{ useNewUrlParser: true }, function(err, client) {
-			if(err) {
-				 console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-			}
-			const collection = client.db("ITWS-4500").collection("User");
-			// perform actions on the collection object
-			collection.find({username: req.session.username}).toArray(function(err, result) {
-				if (err) throw err;
-				console.log(result[0]);
-				info.email = result[0].email;
-				res.json(info);			
-			});
-		});
-	}else{
-		info.username = "Not logged in";
-		info.email = "user@univ.edu";
-		res.json(info);
-	}
-	
-});
-
-//returns posts with specific tag and univ id
-app.get('/posts', function(req,res){
-	tag = req.query.tag;
-	univid=req.session.univid;
-
-});
-
 
 http.listen(3000,() => console.log("Running on port 3000"));
