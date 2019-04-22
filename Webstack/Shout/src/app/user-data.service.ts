22db42a97af5bdd2c0fc9038428a2e557eb64fb5
deahln <deahln@rpi.edu>
2019-04-22 18:28:12 -0400

    user data service

--- Webstack/Shout/src/app/user-data.service.ts
+++ Webstack/Shout/src/app/user-data.service.ts
@@ -6,13 +6,29 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class UserDataService {
-
+  private url = "http://localhost:3000";
   constructor(private http: HttpClient) { }
 
   getProfileInfo() {
-    return this.http.get("http://localhost:3000/profileInfo");
+    return this.http.get(this.url + "/profileInfo");
   }
 
-  editProfileInfo() {}
+  changeUsername(new_username, password) {
+    var new_info = {
+      new_username : new_username,
+      password : password
+    };
+    return this.http.post(this.url + "/updateUsername", new_info);
+  }
+
+  changePassword(username, old_password, new_password){
+    var new_info ={
+      username : username,
+      old_password: old_password,
+      new_password : new_password
+    };
+
+    return this.http.post(this.url + "/updatePassword", new_info);
+  }
 
 }
