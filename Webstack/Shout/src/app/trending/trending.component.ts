0c290366d8dbed93ac11de370821272ba7018488
Matilda Rehm <mathilde.rehm@gmail.com>
2019-04-22 18:02:45 -0400

    fixed filter ul

--- Webstack/Shout/src/app/trending/trending.component.ts
+++ Webstack/Shout/src/app/trending/trending.component.ts
@@ -118,43 +118,6 @@ export class TrendingComponent implements OnInit {
       }
 
 
-    var closeIcon =   document.querySelectorAll('svg.close'),
-                      $container = $('.filter-container'),
-                      $list = $container.find('ul'),
-                      $links = $container.find('a'),
-                      $text = $container.find('span');
-
-    // When the '+' icon is clicked...
-    $(closeIcon).on('click', function() {
-      // Add class to rotate icon to an 'x'
-      (<any> $(this)).toggleClass('active');
-    
-      // Toggle the list
-      $list.toggle(); 
-    });
-
-//     // When the icon is hovered, add
-//     // a class to it's parent so we can style it
-//     $(closeIcon).hover(function() {
-//       $container.addClass('hover');
-//     }, function() {
-//       $container.removeClass('hover');
-//     });
-
-//     $links.on('click', function() {
-//       $links.removeClass('active');
-//       $(this).addClass('active');
-//       $text.text($(this).text()).addClass('fade');
-
-//       setTimeout(function(){
-//         $text.removeClass('fade');
-//       }, 800);
-
-//       $list.toggle();
-//       (<any>closeIcon).toggleClass('active');
-// });
-
-
 
   
 });
@@ -197,12 +160,13 @@ export class TrendingComponent implements OnInit {
     
     if(hasClass) {
       this.renderer.removeClass(event.target, 'active');
+      $('.filter-container').find('ul').toggle();
       // this.renderer.addClass("close");
     } 
     else {
       // this.renderer.removeClass(event.target, "close");
       this.renderer.addClass(event.target, 'active');
-      this.allfilters.nativeElement.toggle();
+      $('.filter-container').find('ul').toggle();
     }
   }
 
