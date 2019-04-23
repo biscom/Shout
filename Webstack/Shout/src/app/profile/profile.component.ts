8e3104f14f2130d45096c1409f2d622a43f2989c
hhy4285 <yi998hh@gmail.com>
2019-04-23 01:50:08 -0400

    profile username and password change update

--- Webstack/Shout/src/app/profile/profile.component.ts
+++ Webstack/Shout/src/app/profile/profile.component.ts
@@ -3,7 +3,7 @@ import { NgModule }       from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 // import { RouterModule, Routes }  from '@angular/router';
 // import { AppRoutingModule } from '../app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, Validators } from '@angular/forms';
 import { UserDataService } from './../user-data.service';
 // import { Observable } from 'rxjs';
 // import { HttpModule } from '@angular/http';
@@ -24,13 +24,24 @@ export class ProfileComponent implements OnInit {
   public show_edit : boolean = false; // show or hide edit button
   public at_edit : boolean = true; // at "About" section or not
 
+  // regular profile info display
   private username;
   private email;
   private password = "";
 
   private savedPosts = [];
 
-  constructor(private userDataService: UserDataService) { }
+  // edit option
+  private editForm;
+  // private new_username;
+  // private old_password;
+  // private new_password1;
+  // private new_password2;
+
+
+
+
+  constructor(private userDataService: UserDataService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.getUserInfo();
@@ -38,7 +49,7 @@ export class ProfileComponent implements OnInit {
 
   getUserInfo() {
     this.userDataService.getProfileInfo()
-    .subscribe((res: any[]) =>{
+    .subscribe((res: any[]) => {
       var data = JSON.parse(JSON.stringify(res));
       this.username = data.username;
       this.email = data.email;
@@ -48,8 +59,41 @@ export class ProfileComponent implements OnInit {
 
   edit_profile() {
     this.show_edit = !this.show_edit;
+
+    this.editForm = this.fb.group({
+      new_username : [''],
+      old_password : ['', Validators.required],
+      new_password1 : [''],
+      new_password2 : ['']
+    });
   }
   save_profile() {
+    if (this.editForm.get('new_username').value != '') {
+      this.userDataService.changeUsername(
+        this.editForm.get('new_username').value,
+        this.editForm.get('old_password').value);
+    }
+
+    if (this.editForm.get('new_password1').value != '' ||
+      this.editForm.get('new_password2').value != ''){
+      // if ()
+      // need check user old password
+      if (this.editForm.get('new_password1').value !=
+      this.editForm.get('new_password2').value) {
+        // alert passwords don't match
+      }
+      else { // password match passed
+        if (this.editForm.get('new_username').value != '') {
+          // use new username
+          this.userDataService.changePassword(new_username, old_password, new_password1);
+        }
+        else {
+          // no new username input, use old username
+          this.userDataService.changePassword(this.username, old_password, new_password1);
+        }
+      }
+    }
+
     this.show_edit = !this.show_edit;
   }
   cancel_edit() {
